f97bf4e3d74563414237eeac21ba325e235510b3
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-24 16:47:38 -0300

    fixing the path

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 
 // send the user to index html page inspite of the url
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname + '/public/index.html'));
 });
 
